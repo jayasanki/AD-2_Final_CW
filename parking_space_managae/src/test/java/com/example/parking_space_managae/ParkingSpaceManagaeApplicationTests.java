181b6f1ef5cf6587478d2ea9f592f7886fb99f1e
nethranjalijayasanki <ssunflower620@gmail.com>
2025-06-25 15:06:32 +0530

    create parking speace manage module

---
+++ parking_space_managae/src/test/java/com/example/parking_space_managae/ParkingSpaceManagaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parking_space_managae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpaceManagaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
