94d045dfcd46649acd97649b689667b4fed0abc2
nethranjalijayasanki <ssunflower620@gmail.com>
2025-06-25 10:23:56 +0530

    create vehicle manage module

---
+++ vehicle_manage/src/main/java/com/example/vehicle_manage/VehicleManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleManageApplication.class, args);
+    }
+
+}
---
+++ vehicle_manage/src/test/java/com/example/vehicle_manage/VehicleManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
