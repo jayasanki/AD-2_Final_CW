26770be7811556d1e9adae0ae07a743a4aa5655d
nethranjalijayasanki <ssunflower620@gmail.com>
2025-06-25 15:18:00 +0530

    create payment manage module

---
+++ payment_manage/src/test/java/com/example/payment_manage/PaymentManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payment_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
